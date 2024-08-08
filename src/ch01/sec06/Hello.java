548d4916008d15f63295fff60b479db20ecfd124
juseok2 <wntjrwnstjr@naver.com>
2024-08-08 10:25:15 +0900

    java_start

---
+++ src/ch01/sec06/Hello.java
@@ -0,0 +1,12 @@
+package ch01.sec06;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("I can push to github");
+		int a = 10;int b = 1;
+		int result = a + b;
+		System.out.println(result);
+	}
+
+}
---
+++ src/ch01/sec11/Calculator.java
@@ -0,0 +1,13 @@
+package ch01.sec11;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 127;
+		int y = 50;
+		int result = x + y;
+		System.out.println(result);
+	}
+
+}
