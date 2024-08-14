e8be9ecaf897de8076198fcc0dfd7e188d789684
juseok2 <wntjrwnstjr@naver.com>
2024-08-14 09:45:32 +0900

    깃

--- src/ch06/sec15/SingletonExample.java
+++ src/ch06/sec15/SingletonExample.java
@@ -9,7 +9,7 @@ public class SingletonExample {
 		Singleton obj2 = Singleton.getInstance();
 		
 		if(obj1 == obj2) {
-			System.out.println("같은 Singleton 객체입니다.");
+			System.out.println("같은 Singleton 객체입니다");
 		}else {
 			System.out.println("다른 Singleton 객체입니다.");
 		}
